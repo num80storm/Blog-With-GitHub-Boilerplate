cc438fbec03292feb678047254a3797f79954812
num80storm <55497437+num80storm@users.noreply.github.com>
2019-12-25 22:14:42 +1100

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,13 +14,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "num80storm的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "num80storm"
+email = "michaelzhu428@yahoo.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+description = "If you make friends with yourself, you will never be alone."
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
